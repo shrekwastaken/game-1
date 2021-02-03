ef0797cdd018f02ea380ce43ca9e44b6d0c41d4a
shrekwastaken <77726844+shrekwastaken@users.noreply.github.com>
2021-02-03 10:31:06 +0000

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -22,25 +22,35 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         .444ee..eeee........
         `)
 })
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    ammo_in_mag = 11
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    projectile = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . 2 2 2 2 2 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, mySprite, 50, 0)
+    if (ammo_in_mag > 0) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . 2 2 2 2 2 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, 50, 0)
+        music.pewPew.play()
+        ammo_in_mag += -1
+    } else {
+        mySprite.say("reload with B", 1000)
+        music.powerDown.play()
+    }
 })
 controller.up.onEvent(ControllerButtonEvent.Released, function () {
     mySprite.setImage(img`
@@ -66,10 +76,8 @@ controller.up.onEvent(ControllerButtonEvent.Released, function () {
         ....ee..eeee........
         `)
 })
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-	
-})
 let projectile: Sprite = null
+let ammo_in_mag = 0
 let mySprite: Sprite = null
 scene.setBackgroundColor(13)
 tiles.setTilemap(tilemap`level1`)
@@ -98,3 +106,37 @@ mySprite = sprites.create(img`
     `, SpriteKind.Player)
 controller.moveSprite(mySprite)
 mySprite.ay = 200
+scene.cameraFollowSprite(mySprite)
+let mySprite2 = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........fffff.........
+    ........ff1111bff.......
+    .......fb1111111bf......
+    .......f111111111f......
+    ......fd1111111ffff.....
+    ......fd111dd1c111bf....
+    ......fb11fcdf1b1bff....
+    ......f11111bfbfbff.....
+    ......f1b1bdfcffff......
+    ......fbfbfcfcccf.......
+    ......ffffffffff........
+    .........ffffff.........
+    .........ffffff.........
+    .........fffffff..f.....
+    ..........fffffffff.....
+    ...........fffffff......
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Enemy)
+mySprite2.setPosition(250, 100)
+let enmy_hp = 2
+mySprite.setVelocity(50, 50)
+ammo_in_mag = 11
+game.onUpdateInterval(1000, function () {
+	
+})
